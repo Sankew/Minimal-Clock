44dccf550883986989ba18cc7433d20a9135b84e
Sankew <sankeshwarsivakumar@gmail.com>
2022-01-22 14:29:54 +0400

    Initial Commit

---
+++ Minimal Clock/Minimal_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minimal_ClockApp.swift
+//  Minimal Clock
+//
+//  Created by Sankeshwar Sivakumar  on 22/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Minimal_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
